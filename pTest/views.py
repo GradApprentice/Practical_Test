0244fab9f85d1e490aae18745ffbd620751f01db
Simon Hodgson <i38174@verisk.com>
2023-03-21 16:22:29 +0000

    Chapter 4

--- pTest/views.py
+++ pTest/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 from pTest import views
 
 def index(request):
-    return HttpResponse("pTest is here!")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Ok!'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'pTest/index.html', context=context_dict)
